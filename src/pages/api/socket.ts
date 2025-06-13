5c6147999b7334456c8c6e4c6ec70f2469e8d30c
Dipesh Rajoria <dipeshrajoria45@gmail.com>
2025-06-13 20:26:31 +0530

    Switch to socket.io for whiteboard sync

---
+++ src/pages/api/socket.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Server } from 'socket.io';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+type NextApiResponseWithSocket = NextApiResponse & {
+  socket: any;
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponseWithSocket) {
+  if (!res.socket.server.io) {
+    const io = new Server(res.socket.server, {
+      path: '/api/socket',
+    });
+
+    io.on('connection', (socket) => {
+      socket.on('join', (roomId: string) => {
+        socket.join(roomId);
+      });
+      socket.on('draw', ({ roomId, x, y }) => {
+        socket.to(roomId).emit('draw', { x, y });
+      });
+    });
+
+    res.socket.server.io = io;
+  }
+  res.end();
+}
